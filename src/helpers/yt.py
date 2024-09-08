bf9bdab8bee8cf727b4ec40a54eb522acc871267
dKosarevsky <36589322+dKosarevsky@users.noreply.github.com>
2024-09-09 02:56:10 +0300

    pogen

--- src/helpers/yt.py
+++ src/helpers/yt.py
@@ -14,29 +14,37 @@ def sort_resolutions(resolutions: list[str], reverse: bool = True) -> list[str]:
     return sorted(set(resolutions), key=lambda x: int(x[:-1]), reverse=reverse)
 
 
-@st.cache_data
-def search_yt_resolution(video_url: str, progressive: bool) -> list[str]:
+def get_yt_obj(url: str) -> YouTube:
     try:
-        video = YouTube(url=video_url, use_po_token=True)
-        resolutions = [i.resolution for i in video.streams.filter(mime_type=MIME, progressive=progressive)]
-        return sort_resolutions(resolutions)
+        return YouTube(
+            url=url,
+            use_po_token=True,
+            po_token=st.secrets.yt.po_token,
+            visitor_data=st.secrets.yt.visitor_data,
+            on_progress_callback=on_progress,
+        )
     except (URLError, RegexMatchError, VideoUnavailable) as err:
         st.error(err)
 
 
-def prepare_yt_video(video_url: str, resolution: str, progressive: bool) -> str | None:
+@st.cache_data
+def search_yt_resolution(yt_obj: YouTube, progressive: bool) -> list[str]:
+    resolutions = [i.resolution for i in yt_obj.streams.filter(mime_type=MIME, progressive=progressive)]
+    return sort_resolutions(resolutions)
+
+
+def prepare_yt_video(yt_obj: YouTube, resolution: str, progressive: bool) -> str | None:
     with st.form('prepare_yt_video'):
         if st.form_submit_button('Prepare Video'):
             with st.spinner('Preparing Video ...'):
-                try:
-                    video = YouTube(url=video_url, use_po_token=True)
-                    if video:
-                        title = video.title
+                try:  # TODO: drop try block here
+                    if yt_obj:
+                        title = yt_obj.title
                         st.write(f'Title: `{title}`')
-                        st.write(f'Publish Date: `{video.publish_date}`')
-                        st.write(f'Duration: `{Duration(seconds=video.length)}`')
-                        st.write(f'Views: `{video.views}`')
-                        video.streams.filter(
+                        st.write(f'Publish Date: `{yt_obj.publish_date}`')
+                        st.write(f'Duration: `{Duration(seconds=yt_obj.length)}`')
+                        st.write(f'Views: `{yt_obj.views}`')
+                        yt_obj.streams.filter(
                             res=resolution,
                             progressive=progressive,
                         ).first().download(
@@ -51,11 +59,12 @@ def prepare_yt_video(video_url: str, resolution: str, progressive: bool) -> str 
 
 def download_yt_video(url: str):
     show_video(data=url)
+    yt_obj = get_yt_obj(url=utl)
     with st.spinner('Update Resolutions List ...'):
         c1, c2, _ = st.columns(3)
         progressive_res = c1.checkbox(label='Use Progressive Resolutions', value=True)
-        resolutions = search_yt_resolution(video_url=url, progressive=progressive_res)
+        resolutions = search_yt_resolution(yt_obj=yt_obj, progressive=progressive_res)
         resolution = c2.selectbox(label='Select Video Resolution:', options=resolutions or [])
 
-    title = prepare_yt_video(video_url=url, resolution=resolution, progressive=progressive_res)
+    title = prepare_yt_video(yt_obj=yt_obj, resolution=resolution, progressive=progressive_res)
     download_video_locally(title=f'{title} {resolution}')
